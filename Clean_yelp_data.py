14b584f6b20b02d4324b903e57d9825008e352db
hzhangal <hzhangal0330@gmail.com>
2018-03-12 20:23:06 +0800

    sample data

--- Clean_yelp_data.py
+++ Clean_yelp_data.py
@@ -2,16 +2,11 @@ import json
 
 review_data_location = '/home/data/corpora/yelp_dataset_challenge_round11/dataset/review.json'
 # sampled_round11_data = list()
-# with open(review_data_location, 'r') as f:
-#     counter = 0
-#     for line in f:
-#         tmp_data = json.loads(line)
-#         sampled_round11_data.append(tmp_data)
-#         counter += 1
-#         if counter >= 100:
-#             break
+with open(review_data_location, 'r') as f:
+    counter = 0
+    for line in f:
+        counter += 1
 
-with open('sampled_yelp_round11.json', 'r') as f:
-    sampled_round_data = json.load(f)
+print('Total number of review: ', counter)
 
 print('end')

e7aac623a40385645ec83b858b99185631f89f65
hzhangal <hzhangal0330@gmail.com>
2018-03-29 13:57:08 +0800

    sample data

---
+++ Label_data_preparation.py
@@ -0,0 +1,119 @@
+from nltk.corpus import wordnet as wn
+from nltk.corpus import verbnet
+import pandas
+import json
+import xml.etree.ElementTree as etree
+import os
+import random
+
+
+def filter_word(input_word):
+    tmp_output = ''
+    for c in input_word:
+        if c in 'zxcvbnmasdfghjklqwertyuiopZXCVBNMASDFGHJKLQWERTYUIOP':
+            tmp_output += c
+    return tmp_output
+
+
+frequency_data = pandas.read_csv('verb_data/verb_frequency.csv')
+frequent_verbs = list()
+for index, row in frequency_data.iterrows():
+    # test = row['Word']
+    test = filter_word(row[1])
+    frequent_verbs.append(test)
+    # print('lalala')
+# print(frequency_data)
+
+
+# test = wn.all_synsets(pos='v')
+# verb = list()
+# for verb_synset in wn.all_synsets(pos='v'):
+#     # verb.append(verb_synset)
+#     verb.append(verb_synset._name.split('.')[0])
+# print(len(verb))
+# new_verb = set(verb)
+# print(len(new_verb))
+
+# test_example = 'verb_data/new_vn/eat-39.1.xml'
+# category = test_example.split('-')[1].split('.')[0]
+verb_dict = dict()
+
+for f_name in os.listdir('verb_data/new_vn'):
+    file_name = 'verb_data/new_vn/' + f_name
+    name_verb = f_name.split('-')[0]
+    category = file_name.split('-')[1].split('.')[0]
+    if category not in verb_dict:
+        verb_dict[category] = list()
+    verb_dict[category].append(name_verb)
+    with open(file_name, 'r') as f:
+        raw_data = f.read()
+        tmp = raw_data.split('name=')
+        for tmp2 in tmp[1:]:
+            verb_dict[category].append(tmp2[1:].split('"')[0])
+
+selected_all_verbs = list()
+all_verbnet_verbs = list()
+limitation = 3
+added_verbs = list()
+
+for category in verb_dict:
+    tmp_verbs = verb_dict[category]
+    contained_verbs = list()
+    for v in tmp_verbs:
+        if v in frequent_verbs:
+            contained_verbs.append(v)
+    random.shuffle(tmp_verbs)
+    contained_verbs = contained_verbs + tmp_verbs
+    for v in contained_verbs[:limitation]:
+        if v not in frequent_verbs:
+            added_verbs.append(v)
+    # if len(tmp_verbs) < limitation:
+    #     for verb in tmp_verbs:
+    #         selected_all_verbs.append(verb)
+    # else:
+    #     selected_tmp_verbs = list()
+    #     for v in frequent_verbs:
+    #         if v in tmp_verbs:
+    #             selected_tmp_verbs.append(v)
+    #         if len(selected_tmp_verbs) >= limitation:
+    #             break
+    #     if len(selected_tmp_verbs) < limitation:
+    #         random.shuffle(tmp_verbs)
+    #         selected_tmp_verbs = selected_tmp_verbs + tmp_verbs
+    #         selected_tmp_verbs = selected_tmp_verbs[:limitation]
+    #     for v in selected_tmp_verbs:
+    #         selected_all_verbs.append(v)
+
+print(len(added_verbs))
+test_verbs = frequent_verbs + added_verbs
+# test_verbs = list()
+# for v in frequent_verbs:
+#     if v in all_verbnet_verbs:
+#         test_verbs.append(v)
+# print(test_verbs)
+
+with open('selected_verbs.json', 'w') as f:
+    json.dump(test_verbs, f)
+
+
+with open('Wino_verb.json', 'r') as f:
+    wino_data = json.load(f)
+#
+print('We analyzing PDP')
+PDP_data = wino_data['PDP']
+PDP_covered_verbs = list()
+for verb in PDP_data:
+    if verb in test_verbs:
+        PDP_covered_verbs.append(verb)
+
+print('We analyzing Wino')
+Wino_data = wino_data['Wino']
+Wino_covered_verbs = list()
+for verb in Wino_data:
+    if verb in test_verbs:
+        Wino_covered_verbs.append(verb)
+#
+print(len(PDP_covered_verbs), len(PDP_data), len(PDP_covered_verbs)/len(PDP_data))
+print(len(Wino_covered_verbs), len(Wino_data), len(Wino_covered_verbs)/len(Wino_data))
+
+print('end')
--- relation_extraction.py
+++ relation_extraction.py
@@ -1,39 +1,67 @@
 import json
+import os
 
-tmp_file_name = '/home/data/corpora/wikipedia/stanford_enhanced++_parsed_data/1000000.json'
 
-with open(tmp_file_name, 'r') as original_f:
-    sampled_data = json.load(original_f)
+def counting_pairs_from_parsed_data(parsed_data, verb_nsubj_amod_dict, verb_dobj_amod_dict, verb_nsubj_dict, verb_dobj_dict):
+    for sentence in parsed_data:
+        for pair in sentence:
+            if pair[1] == 'nsubj':
+                tmp_verb = pair[0][1]
+                tmp_subj = pair[2][1]
+                if tmp_verb not in verb_nsubj_dict:
+                    verb_nsubj_dict[tmp_verb] = dict()
+                if tmp_subj not in verb_nsubj_dict[tmp_verb]:
+                    verb_nsubj_dict[tmp_verb][tmp_subj] = 0
+                verb_nsubj_dict[tmp_verb][tmp_subj] += 1
+                for tmp_pair in sentence:
+                    if tmp_pair[1] == 'amod' and tmp_pair[0][0] == pair[2][0]:
+                        tmp_adj = tmp_pair[2][1]
+                        if tmp_verb not in verb_nsubj_amod_dict:
+                            verb_nsubj_amod_dict[tmp_verb] = dict()
+                        if tmp_adj not in verb_nsubj_amod_dict[tmp_verb]:
+                            verb_nsubj_amod_dict[tmp_verb][tmp_adj] = 0
+                        verb_nsubj_amod_dict[tmp_verb][tmp_adj] += 1
+            if pair[1] == 'dobj':
+                tmp_verb = pair[0][1]
+                tmp_dobj = pair[2][1]
+                if tmp_verb not in verb_dobj_dict:
+                    verb_dobj_dict[tmp_verb] = dict()
+                if tmp_dobj not in verb_dobj_dict[tmp_verb]:
+                    verb_dobj_dict[tmp_verb][tmp_dobj] = 0
+                verb_dobj_dict[tmp_verb][tmp_dobj] += 1
+                for tmp_pair in sentence:
+                    if tmp_pair[1] == 'amod' and tmp_pair[0][0] == pair[2][0]:
+                        tmp_adj = tmp_pair[2][1]
+                        if tmp_verb not in verb_dobj_amod_dict:
+                            verb_dobj_amod_dict[tmp_verb] = dict()
+                        if tmp_adj not in verb_dobj_amod_dict[tmp_verb]:
+                            verb_dobj_amod_dict[tmp_verb][tmp_adj] = 0
+                        verb_dobj_amod_dict[tmp_verb][tmp_adj] += 1
 
+
+print('Start to count the yelp dataset')
+# tmp_file_name = '/home/data/corpora/wikipedia/stanford_enhanced++_parsed_data/1000000.json'
 verb_nsubj_amod_dict = dict()
 verb_dobj_amod_dict = dict()
-for sentence in sampled_data:
-    for pair in sentence:
-        if pair[1] == 'nsubj':
-            for tmp_pair in sentence:
-                if tmp_pair[1] == 'amod' and tmp_pair[0][0] == pair[2][0]:
-                    tmp_verb = pair[0][1]
-                    tmp_adj = tmp_pair[2][1]
-                    if tmp_verb not in verb_nsubj_amod_dict:
-                        verb_nsubj_amod_dict[tmp_verb] = dict()
-                    if tmp_adj not in verb_nsubj_amod_dict[tmp_verb]:
-                        verb_nsubj_amod_dict[tmp_verb][tmp_adj] = 0
-                    verb_nsubj_amod_dict[tmp_verb][tmp_adj] += 1
-        if pair[1] == 'dobj':
-            for tmp_pair in sentence:
-                if tmp_pair[1] == 'amod' and tmp_pair[0][0] == pair[2][0]:
-                    tmp_verb = pair[0][1]
-                    tmp_adj = tmp_pair[2][1]
-                    if tmp_verb not in verb_dobj_amod_dict:
-                        verb_dobj_amod_dict[tmp_verb] = dict()
-                    if tmp_adj not in verb_dobj_amod_dict[tmp_verb]:
-                        verb_dobj_amod_dict[tmp_verb][tmp_adj] = 0
-                    verb_dobj_amod_dict[tmp_verb][tmp_adj] += 1
+verb_nsubj_dict = dict()
+verb_dobj_dict = dict()
 
-with open('verb_nsubj_amod_dict.json', 'w') as nsubj_f:
-    json.dump(verb_nsubj_amod_dict, nsubj_f)
+for name in os.listdir('parsed_yelp_data_with_stanford'):
+    file_name = 'parsed_yelp_data_with_stanford/' + name
+    with open(file_name, 'r') as original_f:
+        sampled_data = json.load(original_f)
+        counting_pairs_from_parsed_data(sampled_data, verb_nsubj_amod_dict, verb_dobj_amod_dict, verb_nsubj_dict, verb_dobj_dict)
 
-with open('verb_dobj_amod_dict.json', 'w') as dobj_f:
-    json.dump(verb_dobj_amod_dict, dobj_f)
+with open('verb_nsubj_amod_dict.json', 'w') as f:
+    json.dump(verb_nsubj_amod_dict, f)
+
+with open('verb_dobj_amod_dict.json', 'w') as f:
+    json.dump(verb_dobj_amod_dict, f)
+
+with open('verb_nsubj_dict.json', 'w') as f:
+    json.dump(verb_nsubj_dict, f)
+
+with open('verb_dobj_dict.json', 'w') as f:
+    json.dump(verb_dobj_dict, f)
 
 print('end')

5af413386897b8f9830ddc9468ee5c4bf6a6490d
hzhangal <hzhangal0330@gmail.com>
2018-03-29 14:11:42 +0800

    sample data

--- relation_extraction.py
+++ relation_extraction.py
@@ -2,43 +2,45 @@ import json
 import os
 
 
-def counting_pairs_from_parsed_data(parsed_data, verb_nsubj_amod_dict, verb_dobj_amod_dict, verb_nsubj_dict, verb_dobj_dict):
+def counting_pairs_from_yelp_parsed_data(parsed_data, verb_nsubj_amod_dict, verb_dobj_amod_dict, verb_nsubj_dict,
+                                    verb_dobj_dict):
     for i, sentence in enumerate(parsed_data):
-        if i % 10000 == 0:
+        if i % 1000 == 0:
             print('We have counted:', i, '/', len(parsed_data))
-        for pair in sentence:
-            if pair[1] == 'nsubj':
-                tmp_verb = pair[0][1]
-                tmp_subj = pair[2][1]
-                if tmp_verb not in verb_nsubj_dict:
-                    verb_nsubj_dict[tmp_verb] = dict()
-                if tmp_subj not in verb_nsubj_dict[tmp_verb]:
-                    verb_nsubj_dict[tmp_verb][tmp_subj] = 0
-                verb_nsubj_dict[tmp_verb][tmp_subj] += 1
-                for tmp_pair in sentence:
-                    if tmp_pair[1] == 'amod' and tmp_pair[0][0] == pair[2][0]:
-                        tmp_adj = tmp_pair[2][1]
-                        if tmp_verb not in verb_nsubj_amod_dict:
-                            verb_nsubj_amod_dict[tmp_verb] = dict()
-                        if tmp_adj not in verb_nsubj_amod_dict[tmp_verb]:
-                            verb_nsubj_amod_dict[tmp_verb][tmp_adj] = 0
-                        verb_nsubj_amod_dict[tmp_verb][tmp_adj] += 1
-            if pair[1] == 'dobj':
-                tmp_verb = pair[0][1]
-                tmp_dobj = pair[2][1]
-                if tmp_verb not in verb_dobj_dict:
-                    verb_dobj_dict[tmp_verb] = dict()
-                if tmp_dobj not in verb_dobj_dict[tmp_verb]:
-                    verb_dobj_dict[tmp_verb][tmp_dobj] = 0
-                verb_dobj_dict[tmp_verb][tmp_dobj] += 1
-                for tmp_pair in sentence:
-                    if tmp_pair[1] == 'amod' and tmp_pair[0][0] == pair[2][0]:
-                        tmp_adj = tmp_pair[2][1]
-                        if tmp_verb not in verb_dobj_amod_dict:
-                            verb_dobj_amod_dict[tmp_verb] = dict()
-                        if tmp_adj not in verb_dobj_amod_dict[tmp_verb]:
-                            verb_dobj_amod_dict[tmp_verb][tmp_adj] = 0
-                        verb_dobj_amod_dict[tmp_verb][tmp_adj] += 1
+        for subsentence in sentence:
+            for pair in subsentence:
+                if pair[1] == 'nsubj':
+                    tmp_verb = pair[0][1]
+                    tmp_subj = pair[2][1]
+                    if tmp_verb not in verb_nsubj_dict:
+                        verb_nsubj_dict[tmp_verb] = dict()
+                    if tmp_subj not in verb_nsubj_dict[tmp_verb]:
+                        verb_nsubj_dict[tmp_verb][tmp_subj] = 0
+                    verb_nsubj_dict[tmp_verb][tmp_subj] += 1
+                    for tmp_pair in sentence:
+                        if tmp_pair[1] == 'amod' and tmp_pair[0][0] == pair[2][0]:
+                            tmp_adj = tmp_pair[2][1]
+                            if tmp_verb not in verb_nsubj_amod_dict:
+                                verb_nsubj_amod_dict[tmp_verb] = dict()
+                            if tmp_adj not in verb_nsubj_amod_dict[tmp_verb]:
+                                verb_nsubj_amod_dict[tmp_verb][tmp_adj] = 0
+                            verb_nsubj_amod_dict[tmp_verb][tmp_adj] += 1
+                if pair[1] == 'dobj':
+                    tmp_verb = pair[0][1]
+                    tmp_dobj = pair[2][1]
+                    if tmp_verb not in verb_dobj_dict:
+                        verb_dobj_dict[tmp_verb] = dict()
+                    if tmp_dobj not in verb_dobj_dict[tmp_verb]:
+                        verb_dobj_dict[tmp_verb][tmp_dobj] = 0
+                    verb_dobj_dict[tmp_verb][tmp_dobj] += 1
+                    for tmp_pair in sentence:
+                        if tmp_pair[1] == 'amod' and tmp_pair[0][0] == pair[2][0]:
+                            tmp_adj = tmp_pair[2][1]
+                            if tmp_verb not in verb_dobj_amod_dict:
+                                verb_dobj_amod_dict[tmp_verb] = dict()
+                            if tmp_adj not in verb_dobj_amod_dict[tmp_verb]:
+                                verb_dobj_amod_dict[tmp_verb][tmp_adj] = 0
+                            verb_dobj_amod_dict[tmp_verb][tmp_adj] += 1
 
 
 print('Start to count the yelp dataset')
@@ -53,7 +55,8 @@ for name in os.listdir('parsed_yelp_data_with_stanford'):
     print('We are working on file:', file_name)
     with open(file_name, 'r') as original_f:
         sampled_data = json.load(original_f)
-        counting_pairs_from_parsed_data(sampled_data, verb_nsubj_amod_dict, verb_dobj_amod_dict, verb_nsubj_dict, verb_dobj_dict)
+        counting_pairs_from_yelp_parsed_data(sampled_data, verb_nsubj_amod_dict, verb_dobj_amod_dict, verb_nsubj_dict,
+                                        verb_dobj_dict)
 
 with open('verb_nsubj_amod_dict.json', 'w') as f:
     json.dump(verb_nsubj_amod_dict, f)

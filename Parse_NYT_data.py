da0e07387288e89258c0f2b4e53867c7a415479d
hzhangal <hzhangal0330@gmail.com>
2018-03-30 11:00:00 +0800

    sample data

--- Parse_NYT_data.py
+++ Parse_NYT_data.py
@@ -12,14 +12,17 @@ for year in os.listdir(root_folder):
     tmp_year_folder = root_folder + '/' + year
     for month in os.listdir(tmp_year_folder):
         print('We are working on month:', month)
-        tmp_month_folder = tmp_year_folder + '/' + month
         tmp_sentences = list()
-        for file_name in os.listdir(tmp_month_folder):
-            tmp_file_name = tmp_month_folder + '/' + file_name
-            tmp_tree = ET.parse(tmp_file_name)
-            tmp_root = tmp_tree.getroot()
-            for children in tmp_root[1][1][1]:
-                tmp_sentences.append(children.text)
+        tmp_month_folder = tmp_year_folder + '/' + month
+        for day in os .listdir(tmp_month_folder):
+            print('We are working on day:', day)
+            tmp_day_folder = tmp_month_folder + '/' + day
+            for file_name in os.listdir(tmp_day_folder):
+                tmp_file_name = tmp_day_folder + '/' + file_name
+                tmp_tree = ET.parse(tmp_file_name)
+                tmp_root = tmp_tree.getroot()
+                for children in tmp_root[1][1][1]:
+                    tmp_sentences.append(children.text)
         with open('prepared_NYT_data/' + year + '_' + month + '.json', 'w') as f:
             json.dump(tmp_sentences, f)
 
